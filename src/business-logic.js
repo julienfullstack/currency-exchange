f22062577504e168b39bc4e0cf49a18758ae2111
Julien L <julienlenaz96@gmail.com>
2023-08-21 18:43:44 -0400

    WIP

--- src/business-logic.js
+++ src/business-logic.js
@@ -1,40 +1,28 @@
-
-export default class ConvertCurrency {
+export default class CurrencyConverter {
+  
   var amount = document.getElementById("amount").value;
   var currency = document.getElementById("currency").value;
+  const apiKey = process.env.API_KEY;
+  const baseCurrency = "USD";
 
-  const API_URL = process.env.API_KEY;
+  const url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${baseCurrency}`;
 
-  // var apiUrl = "https://v6.exchangerate-api.com/v6/${API_URL}/latest/USD";
-
-  fetch(`https://v6.exchangerate-api.com/v6/${API_URL}/latest/USD`)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      if (data.result === "success") {
-        var conversionRates = data.conversion_rates;
-        var convertedAmount = amount * conversionRates[currency];
-        document.getElementById("result").innerHTML = convertedAmount.toFixed(2);
-      } else {
-        document.getElementById("result").innerHTML = "Error retrieving conversion rates.";
-      }
-    })
-    .catch(function(error) {
-      console.log(error);
+  return fetch(url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    if (data.result === "success") {
+      var conversionRates = data.conversion_rates;
+      var convertedAmount = amount * conversionRates[currency];
+      document.getElementById("result").innerHTML = convertedAmount.toFixed(2);
+    } else {
       document.getElementById("result").innerHTML = "Error retrieving conversion rates.";
-    });
+    }
+  })
+  .catch(function(error) {
+    console.log(error);
+    document.getElementById("result").innerHTML = "Error retrieving conversion rates.";
+  });
 }
 
-// UI Logic
-
-// function handleFormSubmission(event) {
-//   event.preventDefault();
-//   convertCurrency();  
-//   document.getElementById("amount").value = null;
-//   document.getElementById("currency").value = null;
-// }
-
-// window.addEventListener("load", function () {
-//   document.querySelector('form').addEventListener("submit", handleFormSubmission);
-// });
